d332e06acea0f46478eb3b6bee37c879724fa353
Rapha-Cruz <raphajcruz@hotmail.com>
2026-01-16 11:09:09 -0300

    responsividade2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,53 @@
+// import Header from './Header'
+// import Sidebar from './SideBar'
+// import './Layout.css'
+
+// export default function Layout({ children }) {
+//   return (    
+//     <div className="layout">
+//       <Header />
+//       <div className="content">
+//         <Sidebar />        
+//         <main className="conteudo">
+//           {children}
+//         </main>
+//       </div>
+      
+//     </div>
+//   )
+// }
+
+
+import { useState } from 'react'
 import Header from './Header'
 import Sidebar from './SideBar'
 import './Layout.css'
 
 export default function Layout({ children }) {
-  return (    
+  const [menuAberto, setMenuAberto] = useState(false)
+
+  return (
     <div className="layout">
       <Header />
+
+      {/* BOTÃO MENU MOBILE */}
+      <button
+        className="menu-toggle"
+        onClick={() => setMenuAberto(true)}
+      >
+        ☰
+      </button>
+
       <div className="content">
-        <Sidebar />        
+        <Sidebar
+          aberto={menuAberto}
+          onClose={() => setMenuAberto(false)}
+        />
+
         <main className="conteudo">
           {children}
         </main>
       </div>
-      
     </div>
   )
 }
-
-
